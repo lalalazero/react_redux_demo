c34537ec515b1249550134f8a4eaab1b7f34e903
lalalazero <zzzero520@hotmail.com>
2018-11-30 18:20:23 +0800

    第一个 React + Redux 的例子，用 props 传递值，进行父子组件通信

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 class App extends Component {
+
+    add1(){
+        this.props.onAdd1();
+    }
+    minus1(){
+        this.props.onMinus1();
+    }
+    addIfOdd(){
+        this.props.onAddIfOdd();
+    }
+    addSync(){
+        this.props.onAddAsync();
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>你点击了{this.props.value}次</p>
+        <button id='add1' className='button' onClick={this.add1.bind(this)}>加1</button>
+        <button id="minus1" className='button' onClick={this.minus1.bind(this)}>减1</button>
+        <button id="addIfOdd" className='button' onClick={this.addIfOdd.bind(this)}>如果是单数加1</button>
+        <button id="addAsync" className='button' onClick={this.addSync.bind(this)}>两秒后加1</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state, action) => {
+  if(state === undefined){
+      return 0;
+  }else{
+      if(action.type === 'add'){
+          let newState = state + action.payload;
+          return newState;
+      }
+      return state;
 
+  }
+}
+const store = createStore(reducer)
+
+function add1() {
+    store.dispatch({type: 'add', payload: 1})
+}
+function minus1() {
+    store.dispatch({type: 'add', payload: -1})
+}
+
+function addIfOdd() {
+    let old = store.getState()
+    if(old % 2 === 1){
+        store.dispatch({type: 'add', payload: 1})
+    }
+}
+
+function addAsync() {
+    setTimeout(()=>{
+        store.dispatch({type: 'add', payload: 1})
+    },2000)
+}
+
+function render(){
+    ReactDOM.render(<App
+            value={store.getState()}
+            onAdd1={()=>add1()}
+            onMinus1={()=>minus1()}
+            onAddIfOdd={()=>addIfOdd()}
+            onAddAsync={()=>addAsync()}
+            />,
+        document.getElementById('root'));
+}
+
+render()
+store.subscribe(render)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
